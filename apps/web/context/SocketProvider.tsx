04acdb01bd9400d41c7d48a00e71e4e9507b00d3
ayush gupta <guptaayush782@outlook.com>
2025-03-06 19:06:00 +0530

    feat(socket): update socket connection URL to production server

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -41,7 +41,7 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    const _socket = io("http://localhost:8000");
+    const _socket = io("https://pluggedchat.onrender.com:10000");
     _socket.on("message", onMessageRec);
 
     setSocket(_socket);
